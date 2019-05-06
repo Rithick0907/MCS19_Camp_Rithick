1e66be48af2b7be049645536f4dd146475a28469
RithickB <49742736+RithickB@users.noreply.github.com>
2019-05-06 12:27:30 +0530

    Update and rename Day2_reverseString.java to ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mcs_bootcamp;
-
 import java.util.Scanner;
-
-public class Day2_reverseString {
+public class ReverseString {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
@@ -16,6 +9,5 @@ public class Day2_reverseString {
             A[i]=sc.nextInt();
         for(int i=N-1;i>=0;i--)
             System.out.print(A[i]+" ");
-    }
-    
+    }   
 }
